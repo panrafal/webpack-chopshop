096de3dd813187c96d4811836a0c43e1d211ab86
Rafal Lindemann <rl@stamina.pl>
2018-08-28 21:00:36 +0200

    Compress urls with pako (100k -> 2k)

--- src/history.js
+++ src/history.js
@@ -1,9 +1,18 @@
 // @flow
 
-export const encodeUrlStateHash = (state: Object) => btoa(JSON.stringify(state))
+import pako from 'pako'
+
+const DEFLATE_HEADER = 'deflate:'
+export const encodeUrlStateHash = (state: Object) =>
+  btoa(DEFLATE_HEADER + pako.deflate(JSON.stringify(state), {to: 'string', level: 9}))
+
 export const decodeUrlStateHash = (hash: string) => {
   try {
-    const state = JSON.parse(atob(hash))
+    const decoded = atob(hash)
+    const json = decoded.startsWith(DEFLATE_HEADER)
+      ? pako.inflate(decoded.slice(DEFLATE_HEADER.length), {to: 'string'})
+      : decoded
+    const state = JSON.parse(json)
     if (!state || typeof state !== 'object') throw new Error(`Url is not an object`)
     return state
   } catch (error) {
