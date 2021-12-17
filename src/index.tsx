ef56d1bf151a6ea82a971f90f020f8951ab6491b
Rafał Lindemann <rl@stamina.pl>
2021-12-17 18:16:14 +0100

    WIP

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,19 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import AppContainer from "./AppContainer"
 import registerServiceWorker from "./registerServiceWorker"
+import { createTheme, ThemeProvider } from "@material-ui/core"
 
 const rootEl = document.getElementById("root")
+const theme = createTheme()
 
 registerServiceWorker()
-ReactDOM.render(<AppContainer />, rootEl)
+
+function AppRoot() {
+  return (
+    <ThemeProvider theme={theme}>
+      <AppContainer />
+    </ThemeProvider>
+  )
+}
+
+ReactDOM.render(<AppRoot />, rootEl)
